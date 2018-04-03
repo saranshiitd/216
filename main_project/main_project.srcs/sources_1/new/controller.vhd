6085dcd5f5ea408ed7398010d2fcfaea529d5f70
saranshiitd <cs1160326@cse.iitd.ac.in>
2018-04-03 21:24:31 +0530

    decoding instruction

--- main_project/main_project.srcs/sources_1/new/controller.vhd
+++ main_project/main_project.srcs/sources_1/new/controller.vhd
@@ -62,22 +62,59 @@ end controller;
 
 architecture Behavioral of controller is
 type state is (fetch , RdAB , RdBC , RdC , WriteRes ) ; 
-type instruction_type_type is (arith , mul , test ) ; 
+--type instruction_type_type is (arith , mul , test , halfwordTranfer) ; 
+type instruction_type_type is (DP, DT, Branch) ;
+type dpsubclass_type is (mul,arith,tst, NotDP);
+type dtsubclass_type is ( wordTransfer , byteTransfer , halfwordTranfer , NotDT ) ;
 signal Immediate : std_logic ;
 signal arithRd : std_logic_vector(3 downto 0) ; 
 signal arithRn : std_logic_vector(3 downto 0) ; 
 signal arithRm : std_logic_vector(3 downto 0) ; 
 signal arithRs : std_logic_vector(3 downto 0) ;
 signal instruction_type : instruction_type_type ;
+signal dpInstructionSubtype : dpsubclass_type ; 
 signal arithRegNoShift : std_logic ;
 signal arithRegShiftCons : std_logic ; 
-signal arithRegShiftReg : std_logic ; 
+signal arithRegShiftReg : std_logic ;
+signal PrePost : std_logic ; 
+signal UpDown : std_logic ; 
+signal dtInstructionSubtype : dtsubclass_type ; 
+signal loadOrStore : std_logic ; -- 0 then store else load 
+signal ImmediateOffset : std_logic ;
+signal writeBackDT : std_logic ;
+signal 
   
 begin
+    instruction_type <=  DT when ( Instruction(27 downto 26) = "01" or ((Instruction(27 downto 26) = "00" and Instruction(11 downto 8) = "0000" and Instruction(4) = '1' and Instruction(7) = '1' and Instruction(6 downto 5) /= "00" ))) else
+                        Branch when (Instruction(27 downto 26) = "10") else 
+                        DP ;
+    Immediate <= Instruction(25) ; 
+    arithRegShiftCons <= '1' when ( ( instruction_type = DP)   and (dpInstructionSubtype = arith) and ( Instruction(4) = '0') ) else '0' ; 
+    arithRegNoShift <= '1' when (( instruction_type = DP)   and (dpInstructionSubtype = arith) and ( Instruction(11 downto 7) = "00000") and (Instruction(4) = '0') ) else '0'; 
+    arithRegShiftReg <= '1' when (( instruction_type = DP)   and (dpInstructionSubtype = arith) and Instruction(4) = '1') else '0' ; 
+    dpInstructionSubtype <= arith when ( instruction_type = DP and ((Instruction(7) and Instruction(4)) = '0') and (Instruction(24 downto 23) /= "10") ) else
+                        mul when (instruction_type = DP and Instruction(7 downto 4) = "1001" and Immediate = '0') else 
+                        tst when ( instruction_type = DP and Instruction(24 downto 23) = "00" and ((Instruction(7) and Instruction(4)) = '0') ) else
+                        NotDP   ;
+    dtInstructionSubtype <= halfwordTranfer when ((instruction_type = DT) and Instruction(11 downto 8) = "0000" and Instruction(4) = '1' and Instruction(7) = '1' and Instruction(6 downto 5) /= "00" ) else 
+                            byteTransfer when ((instruction_type = DT) and Instruction(22) = '1' ) else
+                            wordTransfer when ((instruction_type = DT ) and Instruction(22) = '0' ) else
+                            NotDT ;     
+    loadOrStore <= Instruction(20) when (instruction_type = DT) else 'Z' ;
 
-    arithRegShiftCons <= '1' when ( (instruction_type = arith) and ( Instruction(4) = '0') ) else '0' ; 
-    arithRegNoShift <= '1' when ((instruction_type = arith) and ( Instruction(11 downto 7) = "00000") and (Instruction(4) = '0') ) ; 
-    arithRegShiftReg <= '1' when ((instruction_type = arith) and )
+    ImmediateOffset <= 'Z' when instruction_type /= DT else
+                        Instruction(25) when ((dpInstructionSubtype = wordTransfer) or (dpInstructionSubtype = byteTransfer) ) else
+                        Instruction(22) when dpInstructionSubtype = halfwordTranfer ;
+
+    PrePost <= 'Z' when instruction_type /= DT else
+                Instruction(24) ;
+    UpDown <= 'Z' when instruction_type /= DT else 
+                Instruction(23) ;
+    writeBackDP <= 'Z' when instruction_type /= DT else
+                   Instruction(21) ;
+
+    
+ 
  
 
 end Behavioral;
